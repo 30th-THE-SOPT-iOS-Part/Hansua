6cfb98f6e2f83927eae66d5c93ad48e001bbebee
Sua Han <sssua-0928@Suaui-MacBookPro.local>
2022-04-21 05:38:11 +0900

    [ADD] 2차 세미나

---
+++ 2nd-seminar/2nd-seminar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2nd-seminar
+//
+//  Created by Sua Han on 2022/04/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
